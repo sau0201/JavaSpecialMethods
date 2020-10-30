40364928e06ee4b1e7384d7bc114b932b4e89cfb
Saurabh Sharma <17959926+sau0201@users.noreply.github.com>
2020-10-30 01:17:23 -0700

    added random method example

---
+++ src/com/saurabh/RandomMethod.java
@@ -0,0 +1,12 @@
+package com.saurabh;
+
+import java.util.*;
+
+public class RandomMethod {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    int num = 25;
+    System.out.println(random.nextInt(num));
+  }
+}
